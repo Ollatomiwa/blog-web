5f5c1c025c15b447f7b708d0931f87e838a8aeaf
ollatomiwa <ajaomoshood@gmail.com>
2025-01-31 23:31:18 +0100

    blog posts dumy data created

---
+++ src/store.js
@@ -0,0 +1,80 @@
+import { reactive } from 'vue';
+
+ const store = reactive({
+    posts: [
+        {
+            id: 1,
+            title: 'Post ',
+            descrription: "This is the first post",
+            date: new Date()
+          },
+          {
+            id: 2,
+            title: 'Post  ',
+            descrription: "This is the second post made today",
+            date: new Date()
+          },
+          {
+            id: 3,
+            title: 'Post ',
+            descrription: "This is the third post",
+            date: new Date()
+          },
+          {
+            id: 4,
+            title: 'Post ',
+            descrription: "This is the fourth post",
+            date: new Date()
+          },
+          {
+            id: 5,
+            title: 'Post ',
+            descrription: "This is the fourth post",
+            date: new Date()
+          },
+          {
+            id: 6,
+            title: 'Post ',
+            descrription: "This is the fourth post",
+            date: new Date()
+          },
+          {
+            id: 7,
+            title: 'Post ',
+            descrription: "This is the fourth post",
+            date: new Date()
+          },
+          {
+            id: 8,
+            title: 'Post ',
+            descrription: "This is the fourth post",
+            date: new Date()
+          },
+          {
+            id: 8,
+            title: 'Post ',
+            descrription: "This is the fourth post",
+            date: new Date()
+          },
+          {
+            id: 9,
+            title: 'Post ',
+            descrription: "This is the fourth post",
+            date: new Date()
+          },
+          {
+            id: 10,
+            title: 'Post ',
+            descrription: "This is the fourth post",
+            date: new Date()
+          },
+          {
+            id: 11,
+            title: 'Post ',
+            descrription: "This is the fourth post",
+            date: new Date()
+          },
+    ]
+})
+
+export default store;
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,5 +1,25 @@
-<template>
+<script setup>
+import { useRoute } from 'vue-router';
+import store from '../store';
 
+
+const route = useRoute()
+
+
+const post = store.posts.find(x => x.id === parseInt(route.params.id))
+
+
+</script>
+
+<template>
+    <p class="text-start text-xl lg:ml-40 sm:ml-20 md:ml-20 cursor-pointer  text-gray-900 hover:text-black w-[100px] pt-[100px] mb-[140px]" @click="$router.push('/')">Go Back</p>
+    <div class="Post container text-center mx-auto ">
+      <h1 class="text-black-500 text-4xl font-bold mb-4">
+           {{ post.title }}
+         </h1>
+         <p class="text-md mb-5">{{ post.date }}</p>
+         <p class="text-1">{{ post.descrription }}</p>
+    </div>
 </template>
 
 

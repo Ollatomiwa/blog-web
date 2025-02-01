29f2725dd5eded244cd9f50180d06a0262b17b4d
ollatomiwa <ajaomoshood@gmail.com>
2025-02-01 16:30:08 +0100

    minor update to logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,9 +8,9 @@ const img = ref(logo);
 </script>
 <template>
       <div class="w-full h-[80px] z-10 bg-white fixed drop-shadow-lg">
-        <div class='flex justify-center items-center w-full h-full md:max-w-[1240px] m-auto'>
+        <div class='flex justify-center items-center w-full h-full md:max-w-[700px] m-auto'>
 
-                <div class='flex items-center'>
+                <div class=' items-center'>
                     <img :src="img" alt="E.A. ADESINA & CO" class='sm:ml-10 ss:ml-10 md:ml-3 opacity-[95%] w-full h-[65px]' />
                 </div>
 

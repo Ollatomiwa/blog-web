6214c79dfffcb48a8f0abb03b39cdba6c1d11d07
ollatomiwa <ajaomoshood@gmail.com>
2025-02-01 16:31:13 +0100

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,7 +11,7 @@ const img = ref(logo);
         <div class='flex justify-center items-center w-full h-full md:max-w-[700px] m-auto'>
 
                 <div class=' items-center'>
-                    <img :src="img" alt="E.A. ADESINA & CO" class='sm:ml-10 ss:ml-10 md:ml-3 opacity-[95%] w-full h-[65px]' />
+                    <img :src="img" alt="E.A. ADESINA & CO" class='object-cover sm:ml-10 ss:ml-10 md:ml-3 opacity-[95%] w-full h-[65px]' />
                 </div>
 
         </div>

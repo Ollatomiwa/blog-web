b499d9b02792c5792d3399cc9f9c0692a224b69c
ollatomiwa <ajaomoshood@gmail.com>
2025-02-04 20:13:03 +0100

    minor

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,8 @@ const img = ref(logo);
 
 </script>
 <template>
-      <div class="w-full h-[67px] z-10 bg-white  drop-shadow-lg">
-        <div class='flex  w-full h-full md:max-w-[1200px] sm:max-w-[500px]  m-auto'>
+      <div class=" h-[67px] z-10 bg-white  drop-shadow-lg">
+        <div class='flex  h-full md:max-w-[1200px] sm:max-w-[500px]  m-auto'>
 
                 <div class=' '>
                     <img :src="img" alt="E.A. ADESINA & CO" class='sm:ml-10 ss:ml-10 md:ml-3 opacity-[95%] w-full h-[65px]' />
